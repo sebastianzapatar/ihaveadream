da67332435686a4f16fa32d281c9cfa4a9e71c82
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2024-04-10 11:29:06 -0500

    Se crea el token

---
+++ src/utils/jwt.util.ts
@@ -0,0 +1,14 @@
+import 'dotenv'
+import { sign,verify } from 'jsonwebtoken';
+const JWT_SECRET=process.env.JWT_SECRET||'Frase';
+export const signToken=async (id:string)=>{
+    const jwt=sign({id},JWT_SECRET,{
+        expiresIn:'2h'
+    });
+    return jwt;
+}
+
+export const verifyToken=async (token:string)=>{
+    const isOk=verify(token,JWT_SECRET);
+    return isOk;
+}
